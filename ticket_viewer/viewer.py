797eb6eef0b611979e5487ad246778f43129bdac
yiming-fang <yf2484@columbia.edu>
2021-11-27 17:34:21 -0500

    add main driver

---
+++ ticket_viewer/viewer.py
@@ -0,0 +1,26 @@
+zendesk = 'https://zcc978.zendesk.com'
+
+def main():
+    
+    while True:
+        print('\nSelect viewer option:')
+        print('* Press 1 to view all tickets')
+        print('* Press 2 to a ticket')
+        print('* Type "quit" to exit')
+        
+        option = input()
+        if option == 'quit':
+            print('\nThanks for using the viewer. Goodbye.')
+            break
+        
+        elif option == '1':
+            list_url = f'{zendesk}/api/v2/tickets?per_page=25'
+            print_list(list_url)
+        
+        elif option == '2':
+            tid = input('Enter ticket number: ')
+            print_single(tid)
+
+if __name__ == "__main__":
+    main()
+    
